83fcf9008a1c529e4482584095fe531515ccb530
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2016-11-25 14:41:01 -0200

    Metodo de recuperar o no atual adicionado

--- src/Iterador.java
+++ src/Iterador.java
@@ -1,7 +1,8 @@
 import java.util.Iterator;
 
 public interface Iterador<T> extends Iterator<T> {
+	void append(T dado, ListaEncadeada<T>.Node node);
 	void append(T dado);
 	void insert(T dado);
-	ListaEncadeada<T>.Node getCurrentNode();
+	ListaEncadeada<T>.Node getCurrent();
 }
