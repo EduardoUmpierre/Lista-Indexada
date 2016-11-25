595edd78ea8358e3538302ecd63dad040754a78c
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2016-11-25 14:43:44 -0200

    Metodo de busca adicionado

--- src/ListaIndexada.java
+++ src/ListaIndexada.java
@@ -30,5 +30,53 @@ public class ListaIndexada {
 		while (iterator.hasNext()) {
 			iterator.next();
 		}
+		
+		ListaEncadeada<Object>.Node s = search(2599);
+		
+		System.out.println(s.getData());
+		System.out.println(s.getNext() != null ? s.getNext().getData() : "");
+	}
+	
+	public static ListaEncadeada<Object>.Node search(int index) {
+		System.out.println(" --- -- --- ");
+		
+		int interacoes = 0;
+		Iterador<Object> indexIterator = indice1.iterator();		
+		ListaEncadeada<Object>.Node e = null;
+		
+		while (indexIterator.hasNext()) {
+			indexIterator.next();
+			
+			interacoes++;
+			
+			if(index <= (int) indexIterator.getCurrent().getData()) {				
+				if(index == (int) indexIterator.getCurrent().getData()) {
+					System.out.println("Numero de interacoes: " + interacoes);
+					
+					return indexIterator.getCurrent().getDownLevel();
+				}
+				
+				if(indexIterator.getCurrent().getPrevious() != null) {
+					e = indexIterator.getCurrent().getPrevious().getDownLevel();
+				} else {
+					e = indexIterator.getCurrent().getDownLevel();
+				}
+				
+				while (index > (int) e.getData()) {					
+					e = e.getNext();
+					
+					interacoes++;
+				}
+				
+				break;
+			}
+		}
+		
+		if(e == null)
+			System.out.println("Item não encontrado.");
+		
+		System.out.println("Numero de interacoes: " + interacoes);
+		
+		return e;
 	}
 }
